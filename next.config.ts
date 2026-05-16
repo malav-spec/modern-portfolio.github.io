6b5d7fb9ab0988cac025e6fdb70b554a43fe1cca
Malav Doshi <doshimalav18@gmail.com>
2026-05-16 17:09:00 -0500

    update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Tells Next.js to generate static HTML/CSS files
+  images: {
+    unoptimized: true, // Required for static exports using image components
+  },
 };
 
 export default nextConfig;
